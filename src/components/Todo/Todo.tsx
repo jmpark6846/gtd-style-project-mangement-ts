c87bc441c925490a37c659c60a547020a56912ef
joonmo.park <jmpark6846@gmail.com>
2019-08-20 21:21:29 +0900

    ㅈㅏ잘한 변경

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
+import styled from 'styled-components'
 
-interface Props {}
+const TodoPane = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid white;
+  border-radius: 5px;
+  margin-bottom: 7px;
 
-const Todo: React.FC<Props> = () => {
-  const [text, setText] = useState('this is a default text')
-  return <div>{text}</div>
+  :hover {
+    color: #000;
+  }
+`
+
+interface Props {
+  todoId: string
+  text: string
+  done: boolean
+  onChange(): void
+}
+
+const Todo: React.FC<Props> = ({ text, done, onChange }) => {
+  const handleClick = useCallback(e => e.stopPropagation(), [])
+  return (
+    <TodoPane>
+      <input
+        type="checkbox"
+        checked={done}
+        onClick={handleClick}
+        onChange={onChange}
+      />
+      {text}
+    </TodoPane>
+  )
 }
 
 export default Todo
