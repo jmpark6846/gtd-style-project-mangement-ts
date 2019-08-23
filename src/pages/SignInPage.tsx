0da8e949c98367b03d1edf50727942cd64f3a94d
joonmo.park <jmpark6846@gmail.com>
2019-08-23 19:33:01 +0900

    signin 페이지 추가

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import { RouteComponentProps } from '@reach/router'
+import React, { useCallback, useState } from 'react'
+import { RouteComponentProps, navigate } from '@reach/router'
 import styled from 'styled-components'
 import { Input, Button } from '../components/Common'
+import { useInputState } from '../hooks/useInputState'
+import { firebaseAuth } from '../db'
 
 const SignInPagePane = styled.div`
   h2.title {
@@ -23,21 +25,39 @@ const SignInPagePane = styled.div`
 interface Props extends RouteComponentProps {}
 
 export const SignInPage: React.FC<Props> = () => {
+  const [email, _setEmail, handleEmailChange] = useInputState('')
+  const [password, _setPassword, handlePasswordChange] = useInputState('')
+  const [message, setMessage] = useState('')
+  const handleSignin = useCallback(async () => {
+    try {
+      await firebaseAuth.signInWithEmailAndPassword(email, password)
+      navigate('projects')
+    } catch (error) {
+      console.error(error)
+      setMessage(error.message)
+    }
+  }, [email, password])
+
   return (
     <SignInPagePane>
       <h2 className="title">Sign In</h2>
       <div className="signin-form">
         <div className="field">
-          <Input placeholder="email" />
+          <Input value={email} onChange={handleEmailChange} placeholder="email" />
         </div>
         <div className="field">
-          <Input placeholder="password" />
+          <Input
+            value={password}
+            type="password"
+            onChange={handlePasswordChange}
+            placeholder="password"
+          />
         </div>
       </div>
-      <div className="message"></div>
+      <div className="message">{message}</div>
       <div className="control-pane">
         <Button>회원가입</Button>
-        <Button>로그인</Button>
+        <Button onClick={handleSignin}>로그인</Button>
       </div>
     </SignInPagePane>
   )
